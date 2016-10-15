3c57c60f5d5a2f33a8423e512a56690b93e03cf0
Pat O'Brien <pat@obrienphoto.net>
2016-10-14 22:24:15 -0400

    Create quotes.sql
    Initial

---
+++ quotes.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS `quotes` (
+  `id` bigint(20) NOT NULL,
+  `nick` varchar(20) NOT NULL DEFAULT '',
+  `host` varchar(100) NOT NULL DEFAULT '',
+  `quote` text NOT NULL,
+  `channel` varchar(50) NOT NULL DEFAULT '',
+  `timestamp` bigint(20) DEFAULT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=1400 DEFAULT CHARSET=latin1;
